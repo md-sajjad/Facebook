6431d21824bfda6a7e6e26fbe788751c65609815
md.sajjad <md.sajjad@gmail.com>
2015-09-21 23:15:06 -0700

    Request View Controller done with UIScrollView and Image Placement concepts done

--- Facebook/RequestViewController.swift
+++ Facebook/RequestViewController.swift
@@ -12,16 +12,26 @@ class RequestViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBOutlet weak var requestsImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        scrollView.contentSize = CGSize(width: 320, height: 1000)
+                scrollView.contentSize = CGSizeMake(320, requestsImageView.image!.size.height)
         
-        scrollView.delegate = self
-
+        
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        scrollView.contentInset.top = 0
+        scrollView.contentInset.bottom = 50
+        scrollView.scrollIndicatorInsets.top = 0
+        scrollView.scrollIndicatorInsets.bottom = 50
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
